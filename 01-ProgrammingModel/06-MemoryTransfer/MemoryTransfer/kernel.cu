50963907bdab3d145c8a7e56c65d504a3c81cd11
Ashish Shelke <exampleemail@example>
2024-02-28 01:19:00 +0530

    CUDA Memory transfer between host and device

---
+++ 01-ProgrammingModel/06-MemoryTransfer/MemoryTransfer/kernel.cu
@@ -0,0 +1,44 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstring>
+
+__global__ void memoryTransferTest(int* input)
+{
+	int gid = threadIdx.x + (blockIdx.x * blockDim.x);
+	//if(gid < size)
+	printf("gid = %d, tid = %d, value = %d \n", gid, threadIdx.x, input[gid]);
+}
+
+int main()
+{
+	int size = 128;
+	int arraySize = size * sizeof(int);
+
+	dim3 block(64);
+	dim3 grid(2);
+
+	int* hostInput = NULL;
+	hostInput = (int*)malloc(arraySize);
+
+	time_t clock;
+	srand((unsigned)time(&clock));
+
+	for (int i = 0; i < size; i++)
+	{
+		hostInput[i] = ((int)rand() & 0xff);
+	}
+
+	int* devInput;
+	cudaMalloc((void**)&devInput, arraySize);
+	cudaMemcpy(devInput, hostInput, arraySize, cudaMemcpyHostToDevice);
+
+	memoryTransferTest << < grid, block>> > (devInput);
+	cudaDeviceSynchronize();
+	cudaDeviceReset();
+	return 0;
+}
