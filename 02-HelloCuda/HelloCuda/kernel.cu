7a079cdbbbbb15c6b84f59530ffd537c73b36f98
Ashish Shelke <exampleemail@example>
2024-02-27 22:57:16 +0530

    CUDA Hello world program

---
+++ 02-HelloCuda/HelloCuda/kernel.cu
@@ -0,0 +1,27 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void hello_cuda()
+{
+	printf("Hello CUDA World !!! \n");
+}
+
+int main()
+{
+	int nx = 16; //Total Threads per block
+	int ny = 4; // Total Blocks per grid
+
+	dim3 block(8, 2, 1); // 4 thread in x direction, 1 in y direction, 1 in z direction
+	dim3 grid(nx/block.x, ny/block.y, 1); // 8 block in x direction, 1 block in y direction, 1 block in z direction
+
+	//Kernal launch << <grid, block>> > 
+	hello_cuda << <grid, block>> > ();
+
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return 0;
+}
